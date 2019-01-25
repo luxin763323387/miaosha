ca7c64b9e0958a394ae7daf71942fc58ddd9339d
StevenLu <luxin763323387@163.com>
2019-01-25 20:27:07 +0800

    简易版验证码发送

--- src/main/java/com/cn/lx/controller/UserController.java
+++ src/main/java/com/cn/lx/controller/UserController.java
@@ -9,19 +9,45 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Random;
+
 @RestController
 @RequestMapping(value = "/user/")
 public class UserController {
 
-
     @Autowired
     private UserService userService;
+    @Autowired
+    private HttpServletRequest httpServletRequest;
 
+    //获取手机验证码
+    @RequestMapping(value = "getOtp",method = RequestMethod.GET)
+    public ResponseVO getOtp(@RequestParam(name="telephone")String telephone){
+        //按照一定规则生成验证码
+        Random random = new Random();
+        int randNum = random.nextInt(9999)+1000;
+        String otp = ""+randNum;
 
-    //通过id获取信息
+        //根据session获取验证码
+        httpServletRequest.getSession().setAttribute(otp,telephone);
+        //将验证码发送给手机
+        System.out.println("telephone："+telephone+" &otp： "+otp);
+        return ResponseVO.success("发送成功");
+    }
+
+    /**
+     * 通过id获取信息
+     * @param id
+     * @return
+     */
     @RequestMapping(value = "getUserInfo",method = RequestMethod.GET)
     public ResponseVO getUserInfo(@RequestParam(name = "id")Integer id){
         UserVO userVO = userService.getUserInfoById(id);
-        return ResponseVO.success(userVO);
+        if(userVO == null){
+            return  ResponseVO.serviceFail("用户不存在");
+        }else {
+            return ResponseVO.success(userVO);
+        }
     }
 }
